d0fa411c948081fe1e78afb133923cfb490d65f0
jviriya <viriya.mon@snocko-tech.com>
2021-12-30 13:49:43 +0700

    test release version

--- money/validator.go
+++ money/validator.go
@@ -8,3 +8,10 @@ func GoValidatorCustomTypeFunc(field reflect.Value) interface{} {
 	}
 	return nil
 }
+
+func GoValidatorCustomTypeFunc2(field reflect.Value) interface{} {
+	if value, ok := field.Interface().(Money); ok {
+		return value.Float64()
+	}
+	return nil
+}
