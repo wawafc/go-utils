615a37422ae48e862b4d232a06c9d1f1a0b51e23
V M <viriya.mon@snocko-tech.com>
2023-08-28 16:10:46 +0700

    test aws Rekognition

---
+++ aws/rekognition.go
@@ -0,0 +1,50 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/rekognition"
+	"github.com/aws/aws-sdk-go-v2/service/rekognition/types"
+)
+
+type Client struct {
+	*rekognition.Client
+}
+
+func NewRekognition(cfg aws.Config) *rekognition.Client {
+	return rekognition.NewFromConfig(cfg)
+
+}
+
+func (cli Client) CompareFace(source, dest []byte) (float32, error) {
+
+	var similar float32
+
+	input := &rekognition.CompareFacesInput{
+		//SimilarityThreshold: aws.Float64(90.000000),
+		SourceImage: &types.Image{
+			Bytes: source,
+		},
+		TargetImage: &types.Image{
+			Bytes: dest,
+		},
+	}
+
+	result, err := cli.CompareFaces(context.Background(), input)
+	if err == nil && len(result.FaceMatches) > 0 {
+		for _, matchedFace := range result.FaceMatches {
+			similar = *matchedFace.Similarity
+			break
+		}
+
+	} else {
+		errMsg := ""
+		if err != nil {
+			errMsg = err.Error()
+			fmt.Println("AWS ERROR:", errMsg)
+		}
+	}
+
+	return similar, err
+}
